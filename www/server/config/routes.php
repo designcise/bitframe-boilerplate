c3375047cf099bc2c4f0439c119cfd11baed99cf
Designcise <designcise@gmail.com>
2020-04-15 02:43:56 +0200

    Updated route controller

--- www/server/config/routes.php
+++ www/server/config/routes.php
@@ -4,5 +4,5 @@ use BitFrame\Router\AbstractRouter;
 use YourProject\App\Controller\DefaultController;
 
 return static function (AbstractRouter $router) {
-    $router->get('/hello/{action}', DefaultController::class);
+    $router->get('/hello/{action}', [DefaultController::class, 'testAction']);
 };
